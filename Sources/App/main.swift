9901fca53f004eef40bb9fa26d9cc35d43011b2b
missyalienn <allan.missy@gmail.com>
2017-03-25 13:21:50 -0400

    playing with vapor

---
+++ Sources/App/main.swift
@@ -0,0 +1,62 @@
+import Vapor
+
+
+
+let drop = Droplet()
+
+drop.get { request  in
+    //return "Hello, Vapor!"
+    return try JSON(node: [
+        "message" : "Hello, world"
+        
+        ])
+}
+
+// at the url localhost8080:/hello
+drop.get("hello") { request  in
+    return try JSON(node: [
+        "message" : "Hello, world! Hi, Again!"
+        
+        ])
+}
+
+//nest paths
+drop.get("hello", "there") { request  in
+    return try JSON(node: [
+        "message" : "Ok, that's enough for now."
+        
+        ])
+}
+
+drop.get("beers", Int.self) { request, beers in
+    return try JSON(node: [
+        
+        "message": "Take one down, pass it around. \(beers - 1) beers on the wall..."
+        ])
+    
+}
+
+drop.post("post")  { request in
+    guard let name = request.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    return try JSON(node: [
+        "message":"Hello, \(name)"
+        ])
+}
+
+
+drop.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
